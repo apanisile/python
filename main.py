019f674685d26c1603b267ec2372413689fe341b
apanisile <apanisileifeoluwa@gmail.com>
2021-02-28 08:31:03 +0100

    added a new module for sign up page for the app

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-#Python program to calculate loan
-
+from existing_user import *
 from user_details import *
 
-prompt_user = input("""Do you want to sign in? (Y)es or (N)o 
+print ("""Press (1) if you are you a new user 
+Press (2) an existing user? """)
+user_type = input("> ")
+
+if (user_type == "1"):
+    username = input("""Please enter the username you would like to use
     > """)
-
-if (prompt_user == "Y") | (prompt_user == "y") | (prompt_user == "yes") | (prompt_user == "Yes"):
-
-    user = loan_function()
-    user.name()
-    user.email()
-
-    print("Your credit core should be more than $500 to qualify for our loan")
-    bank_credit = int(input("Please what is your bank credit score: "))
-
-    bench_mark = 500
-
-    if bank_credit >= bench_mark:
-        user.payment()
-        print("Your account would be credited in the next 2-5 working days")
-
+    print (username)
+    user_pin = input("""Please enter a pin you would like to use
+    > """)
+    for (len(user_pin) != 5):
+        print("Please enter a 5 digit pin")
     else:
-        print(f"Sorry! You do not qualify for our bank loan")
-        exit(1)
+        print("Great!")
 
-else:
-    print("Bye!")
-    exit(0)
+elif (user_type == "2"):
+    old = existing_user()
+    old.old_user()
+    
